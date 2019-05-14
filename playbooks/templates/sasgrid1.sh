d21f3cd6ffc36410e809457f562c2e86a74e987d
Bennie Fiol <Bennie.Fiol@sas.com>
2019-05-14 15:12:23 -0400

    (AWSGRID-28) Modify grid quickstart to use 9.4m6 (18w47) - Modified to start up SASStudio

--- playbooks/templates/sasgrid1.sh
+++ playbooks/templates/sasgrid1.sh
@@ -27,12 +27,21 @@ cd /sas/$1
 ./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/grid1_config.txt 
 exit
 EOF
-# Add back above if this works: ./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/studio_config.txt 
+
+sudo -u root bash << EOF
+/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh 
+exit
+EOF
+
+sudo -u sasinst bash << EOF
+cd /sas/$1                        
+./setup.sh -deploy -quiet -responsefile /sas/quickstart/playbooks/templates/studio_config.txt 
+exit
+EOF
 
 sudo -u root bash << EOF
 echo '-WORK /saswork' >> /sas/SASHome/SASFoundation/9.4/nls/en/sasv9.cfg
+/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh 
+. /sas/studioconfig/sasstudio.sh start
 exit
 EOF
-# Add back above if works.
-#/sas/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh 
-#. /sas/studioconfig/sasstudio.sh start
